9f35bc65e87d5517ff1db4b8b4576ce87cb63316
DeWill404 <mr.irshad.siddique@gmail.com>
2021-03-25 16:54:39 +0530

    createed seperate js fie for product data & render all data from it for index & feature page

---
+++ Shayaan/data.js
@@ -0,0 +1,54 @@
+// Map of data of products
+const DATA = [
+	{
+    "imgSrc": [
+      "1.jpg",
+      "2.jpg",
+      "3.jpg",
+    ],
+    "cardTitle": "Item 1",
+    "subTitle": "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    "rating": 3.0,
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla cupiditate obcaecati excepturi quidem quas? Praesentium autem a nesciunt suscipit libero dignissimos aliquid voluptatibus quibusdam mollitia sunt at odio ullam velit provident repudiandae quos vel sequi perferendis ducimus atque, impedit quae. Ducimus et temporibus accusantium facilis quis id laudantium, quam voluptates.",
+	  "newPrice": 999,
+	  "oldPrice": 1500,
+	  "discount": 30,
+    "color": [
+      "234543",
+      "324332",
+      "432343",
+      "344233",
+      "342344",
+    ],
+    "size": [
+      "Large",
+      "Extra Large",
+    ],
+	}, {
+    "imgSrc": [
+      "4.jpg",
+      "5.jpg",
+      "6.jpg",
+    ],
+    "cardTitle": "Item 2",
+    "subTitle": "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    "rating": 4.5,
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla cupiditate obcaecati excepturi quidem quas? Praesentium autem a nesciunt suscipit libero dignissimos aliquid voluptatibus quibusdam mollitia sunt at odio ullam velit provident repudiandae quos vel sequi perferendis ducimus atque, impedit quae. Ducimus et temporibus accusantium facilis quis id laudantium, quam voluptates.",
+	  "newPrice": 99,
+	  "oldPrice": 100,
+	  "discount": 0,
+    "color": [
+      "FFFFFF",
+      "AAAAAA",
+      "EEEEEE",
+      "DDDDDD",
+      "BBBBBB",
+    ],
+    "size": [
+      "Small",
+      "Medium",
+      "Large",
+      "Extra Large",
+    ],
+	},
+];
